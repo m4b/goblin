05ccc274075519498c007a4ec9b304f4fcbef629
m4b <m4b.github.io@gmail.com>
2016-10-15 19:02:36 -0700

    elf: rename 32/64 bit versions to suffix 32/64 because redundant file names are annoying

--- src/elf/_32/mod.rs
+++ src/elf/_32/mod.rs
@@ -1,16 +1,16 @@
 //! The ELF 32-bit struct definitions and associated values
 
-//#[path="header32.rs"]
+#[path="header32.rs"]
 pub mod header;
-//#[path="sym32.rs"]
+#[path="sym32.rs"]
 pub mod sym;
-//#[path="program_header32.rs"]
+#[path="program_header32.rs"]
 pub mod program_header;
-//#[path="section_header32.rs"]
+#[path="section_header32.rs"]
 pub mod section_header;
-//#[path="dyn32.rs"]
+#[path="dyn32.rs"]
 pub mod dyn;
-//#[path="rela32.rs"]
+#[path="rela32.rs"]
 pub mod rela;
 
 #[cfg(feature = "std")]
--- src/elf/_64/mod.rs
+++ src/elf/_64/mod.rs
@@ -1,16 +1,16 @@
 //! The ELF 64-bit struct definitions and associated values
 
-//#[path="header64.rs"]
+#[path="header64.rs"]
 pub mod header;
-//#[path="sym64.rs"]
+#[path="sym64.rs"]
 pub mod sym;
-//#[path="program_header64.rs"]
+#[path="program_header64.rs"]
 pub mod program_header;
-//#[path="section_header64.rs"]
+#[path="section_header64.rs"]
 pub mod section_header;
-//#[path="dyn64.rs"]
+#[path="dyn64.rs"]
 pub mod dyn;
-//#[path="rela64.rs"]
+#[path="rela64.rs"]
 pub mod rela;
 
 #[cfg(feature = "std")]

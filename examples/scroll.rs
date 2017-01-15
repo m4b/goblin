e2c2c22ec242ecffc1c43f3eba0360162c09731d
m4b <m4b.github.io@gmail.com>
2017-01-14 20:33:55 -0800

    examples: add scroll.rs to demonstrate awesomness of scroll

---
+++ examples/scroll.rs
@@ -0,0 +1,24 @@
+/// Demonstrates the power of scroll
+/// Goblin implements `TryFromCtx` for the header type
+/// which means downstream crates/clients can just "parse" headers out of
+/// arbitrary buffers, without learning new crate specific function names
+/// I.e., all you need are Types + Pread = Happiness
+
+extern crate scroll;
+extern crate goblin;
+
+use goblin::{error, elf64};
+use scroll::Pread;
+
+fn run () -> error::Result<()> {
+    use Pread;
+    let crt1: Vec<u8> = include!("../etc/crt1.rs");
+    let header: elf64::header::Header = crt1.pread_into(0)?;
+    assert_eq!(header.e_type, elf64::header::ET_REL);
+    println!("header: {:?}", &header);
+    Ok(())
+} 
+
+fn main() {
+    run().unwrap();
+}

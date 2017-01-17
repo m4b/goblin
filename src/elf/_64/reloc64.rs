45e406f066748b519a1a59e182cc814ff5b6d53b
m4b <m4b.github.io@gmail.com>
2017-01-16 23:24:36 -0800

    elf.reloc: refactor into reloc.rs with Pread impl

--- src/elf/_64/reloc64.rs
+++ src/elf/_64/reloc64.rs
@@ -20,25 +20,4 @@ pub fn r_info(sym: u64, typ: u64) -> u64 {
     (sym << 32) + typ
 }
 
-elf_rela_impure_impl!(
-
-    #[cfg(feature = "endian_fd")]
-    pub fn parse<S: scroll::Gread>(fd: &S, offset: usize, size: usize, little_endian: scroll::Endian, is_rela: bool) -> Result<Vec<Reloc>> {
-        let sizeof_relocation = if is_rela { SIZEOF_RELA } else { SIZEOF_REL };
-        let count = size / sizeof_relocation;
-        let mut res = Vec::with_capacity(count);
-        let mut offset = offset;
-        for _ in 0..count {
-            let mut reloc = Reloc::default();
-            reloc.r_offset = fd.gread::<u64>(&mut offset, little_endian)? as usize;
-            let info = fd.gread::<u64>(&mut offset, little_endian)?;
-            reloc.r_info = info as usize;
-            if is_rela { reloc.r_addend = fd.gread::<i64>(&mut offset, little_endian)? as isize; }
-            reloc.r_sym = r_sym(info) as usize;
-            reloc.r_type = r_type(info) as u32;
-            reloc.is_rela = is_rela;
-            res.push(reloc);
-        }
-        Ok(res)
-    }
-);
+elf_rela_impure_impl!(u64);
--- src/elf/reloc.rs
+++ src/elf/reloc.rs
@@ -223,7 +223,7 @@ pub fn type_to_str(typ: u32) -> &'static str {
     }
 }
 
-macro_rules! elf_rela_impure_impl { ($parse:item) => {
+macro_rules! elf_rela_impure_impl { ($size:ident) => {
 
         #[cfg(feature = "std")]
         pub use self::impure::*;
@@ -346,6 +346,23 @@ macro_rules! elf_rela_impure_impl { ($parse:item) => {
             }
 
             #[cfg(feature = "endian_fd")]
-            $parse
+            pub fn parse<S: scroll::Gread>(fd: &S, offset: usize, size: usize, little_endian: scroll::Endian, is_rela: bool) -> Result<Vec<Reloc>> {
+                let sizeof_relocation = if is_rela { SIZEOF_RELA } else { SIZEOF_REL };
+                let count = size / sizeof_relocation;
+                let mut res = Vec::with_capacity(count);
+                let mut offset = offset;
+                for _ in 0..count {
+                    let mut reloc = Reloc::default();
+                    reloc.r_offset = fd.gread::<$size>(&mut offset, little_endian)? as usize;
+                    let info = fd.gread::<$size>(&mut offset, little_endian)?;
+                    reloc.r_info = info as usize;
+                    if is_rela { reloc.r_addend = fd.gread::<signed_from_unsigned!($size)>(&mut offset, little_endian)? as isize; }
+                    reloc.r_sym = r_sym(info) as usize;
+                    reloc.r_type = r_type(info) as u32;
+                    reloc.is_rela = is_rela;
+                    res.push(reloc);
+                }
+                Ok(res)
+            }
         }
     };}

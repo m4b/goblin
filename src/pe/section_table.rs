a15b6f6ec3cab78f69096fe798439a4e91b7cdf3
roblabla <unfiltered@roblab.la>
2018-08-08 13:14:47 +0200

    avoid using newer rust features

--- src/pe/section_table.rs
+++ src/pe/section_table.rs
@@ -27,13 +27,18 @@ fn base64_decode_string_entry(s: &str) -> Result<usize, ()> {
 
     let mut val = 0;
     for c in s.bytes() {
-        let v = match c {
-            b'A'..=b'Z' => c - b'A' + 00, // 00..=25
-            b'a'..=b'z' => c - b'a' + 26, // 26..=51
-            b'0'..=b'9' => c - b'0' + 52, // 52..=61
-            b'+'        => 62,            // 62
-            b'/'        => 63,            // 63
-            _           => return Err(())
+        let v = if b'A' <= c && c <= b'Z' {
+            c - b'A' + 00 // 00..=25
+        } else if b'a' <= c && c <= b'z' {
+            c - b'a' + 26 // 26..=51
+        } else if b'0' <= c && c <= b'9' {
+            c - b'0' + 52 // 52..=61
+        } else if c == b'+' {
+            62            // 62
+        } else if c == b'/' {
+            63            // 63
+        } else {
+            return Err(())
         };
         val = val * 64 + v as usize;
     }

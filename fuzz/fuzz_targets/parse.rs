4e3f8bba6622e5efb39f4879a195d6514ce4c891
Lzu Tao <taolzu@gmail.com>
2019-03-28 02:29:48 +0700

    Fix fuzz build and move it to 2018 edition

--- fuzz/fuzz_targets/parse.rs
+++ fuzz/fuzz_targets/parse.rs
@@ -1,7 +1,6 @@
 #![no_main]
-#[macro_use] extern crate libfuzzer_sys;
-extern crate goblin;
+use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let _ = goblin::parse(data);
+    let _ = goblin::Object::parse(data);
 });

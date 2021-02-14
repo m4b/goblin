612f0406f83e94bf21826df3bd5b8596f7113025
unknown <abramlujan@gmail.com>
2021-02-13 23:49:36 -0300

    pe.data_directories: replace unsafe get_unchecked calls with direct indexing

--- src/pe/data_directories.rs
+++ src/pe/data_directories.rs
@@ -45,62 +45,62 @@ impl DataDirectories {
     }
     pub fn get_export_table(&self) -> &Option<DataDirectory> {
         let idx = 0;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
     pub fn get_import_table(&self) -> &Option<DataDirectory> {
         let idx = 1;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
     pub fn get_resource_table(&self) -> &Option<DataDirectory> {
         let idx = 2;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
     pub fn get_exception_table(&self) -> &Option<DataDirectory> {
         let idx = 3;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
     pub fn get_certificate_table(&self) -> &Option<DataDirectory> {
         let idx = 4;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
     pub fn get_base_relocation_table(&self) -> &Option<DataDirectory> {
         let idx = 5;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
     pub fn get_debug_table(&self) -> &Option<DataDirectory> {
         let idx = 6;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
     pub fn get_architecture(&self) -> &Option<DataDirectory> {
         let idx = 7;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
     pub fn get_global_ptr(&self) -> &Option<DataDirectory> {
         let idx = 8;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
     pub fn get_tls_table(&self) -> &Option<DataDirectory> {
         let idx = 9;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
     pub fn get_load_config_table(&self) -> &Option<DataDirectory> {
         let idx = 10;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
     pub fn get_bound_import_table(&self) -> &Option<DataDirectory> {
         let idx = 11;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
     pub fn get_import_address_table(&self) -> &Option<DataDirectory> {
         let idx = 12;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
     pub fn get_delay_import_descriptor(&self) -> &Option<DataDirectory> {
         let idx = 13;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
     pub fn get_clr_runtime_header(&self) -> &Option<DataDirectory> {
         let idx = 14;
-        unsafe { self.data_directories.get_unchecked(idx) }
+        &self.data_directories[idx]
     }
 }

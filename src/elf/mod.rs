29de1bac592c90070ac8f67ec99fd1a429d4286b
messense <messense@icloud.com>
2021-12-05 16:05:14 +0800

    elf: add runpaths

--- src/elf/mod.rs
+++ src/elf/mod.rs
@@ -126,8 +126,11 @@ if_sylvan! {
         /// A list of this binary's dynamic libraries it uses, if there are any
         pub libraries: Vec<&'a str>,
         /// A list of runtime search paths for this binary's dynamic libraries it uses, if there
-        /// are any
+        /// are any. (deprecated)
         pub rpaths: Vec<&'a str>,
+        /// A list of runtime search paths for this binary's dynamic libraries it uses, if there
+        /// are any.
+        pub runpaths: Vec<&'a str>,
         /// Whether this is a 64-bit elf or not
         pub is_64: bool,
         /// Whether this is a shared object or not
@@ -243,6 +246,7 @@ if_sylvan! {
                 interpreter: None,
                 libraries: vec![],
                 rpaths: vec![],
+                runpaths: vec![],
                 is_64: misc.is_64,
                 is_lib: misc.is_lib,
                 entry: misc.entry,
@@ -301,6 +305,7 @@ if_sylvan! {
             let mut soname = None;
             let mut libraries = vec![];
             let mut rpaths = vec![];
+            let mut runpaths = vec![];
             let mut dynsyms = Symtab::default();
             let mut dynrelas = RelocSection::default();
             let mut dynrels = RelocSection::default();
@@ -323,8 +328,12 @@ if_sylvan! {
                 }
                 for dyn_ in &dynamic.dyns {
                     if dyn_.d_tag == dynamic::DT_RPATH {
-                        if let Some(rpath) = dynstrtab.get_at(dyn_.d_val as usize) {
-                            rpaths.push(rpath);
+                        if let Some(path) = dynstrtab.get_at(dyn_.d_val as usize) {
+                            rpaths.push(path);
+                        }
+                    } else if dyn_.d_tag == dynamic::DT_RUNPATH {
+                        if let Some(path) = dynstrtab.get_at(dyn_.d_val as usize) {
+                            runpaths.push(path);
                         }
                     }
                 }
@@ -383,6 +392,7 @@ if_sylvan! {
                 interpreter,
                 libraries,
                 rpaths,
+                runpaths,
                 is_64: misc.is_64,
                 is_lib: misc.is_lib,
                 entry: misc.entry,

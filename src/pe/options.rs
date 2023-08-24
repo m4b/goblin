0567ee95d83f338ca2aa31d248ced8e3b1c86290
Sutton Bradley <suttonb@microsoft.com>
2023-08-23 21:34:15 -0700

    Add in_memory feature to prevent parsing attribute certificates (not pulled into memory by loader)

--- src/pe/options.rs
+++ src/pe/options.rs
@@ -3,11 +3,22 @@
 pub struct ParseOptions {
     /// Wether the parser should resolve rvas or not. Default: true
     pub resolve_rva: bool,
+    /// Whether or not to parse attribute certificates.
+    /// Set to false for in-memory representation, as the [loader does not map this info into
+    /// memory](https://learn.microsoft.com/en-us/windows/win32/debug/pe-format#other-contents-of-the-file).
+    /// For on-disk representations, leave as true.
+    /// Default: true
+    #[cfg(feature = "in_memory")]
+    pub parse_attribute_certificates: bool,
 }
 
 impl ParseOptions {
     /// Returns a parse options structure with default values
     pub fn default() -> Self {
-        ParseOptions { resolve_rva: true }
+        ParseOptions {
+            resolve_rva: true,
+            #[cfg(feature = "in_memory")]
+            parse_attribute_certificates: true,
+        }
     }
 }

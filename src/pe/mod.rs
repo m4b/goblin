5f93c2650898b7c4eea7ee7b0f58f5776ef96ca3
Sutton Bradley (HE/HIM) <suttonb@microsoft.com>
2023-08-24 05:17:57 -0700

    Make compatible with and without feature on

--- src/pe/mod.rs
+++ src/pe/mod.rs
@@ -226,7 +226,13 @@ impl<'a> PE<'a> {
                 }
             }
 
-            let certtable = if opts.parse_attribute_certificates {
+            // Parse attribute certificates unless opted out of via
+            // the in_memory feature and the parse_attribute_certificates parsing option.
+            #[cfg(feature = "in_memory")]
+            let parse_attribute_certificates = opts.parse_attribute_certificates;
+            #[cfg(not(feature = "in_memory"))]
+            let parse_attribute_certificates = true;
+            let certtable = if parse_attribute_certificates {
                 if let Some(certificate_table) =
                     *optional_header.data_directories.get_certificate_table()
                 {

3d1d72552deab24f4832fd2b89a50e98bdb1e448
Raito Bezarius <masterancpp@gmail.com>
2024-01-23 02:46:50 +0100

    pe(utils): generic alignment tool
    This is a generic function to perform various alignments, this avoids reinventing
    locally the magic alignment dance and reusable for consumers who may themselves align things.

--- src/pe/utils.rs
+++ src/pe/utils.rs
@@ -22,6 +22,23 @@ fn aligned_pointer_to_raw_data(pointer_to_raw_data: usize) -> usize {
     pointer_to_raw_data & !PHYSICAL_ALIGN
 }
 
+// Performs arbitrary alignment of values
+// based on homogeneous numerical types.
+#[inline]
+pub fn align_to<N>(value: N, align: N) -> N
+where
+    N: core::ops::Add<Output = N>
+        + core::ops::Not<Output = N>
+        + core::ops::BitAnd<Output = N>
+        + core::ops::Sub<Output = N>
+        + core::cmp::PartialEq
+        + core::marker::Copy,
+    u8: Into<N>,
+{
+    debug_assert!(align != 0u8.into(), "Align must be non-zero");
+    return (value + align - 1u8.into()) & !(align - 1u8.into());
+}
+
 #[inline]
 fn section_read_size(section: &section_table::SectionTable, file_alignment: u32) -> usize {
     fn round_size(size: usize) -> usize {

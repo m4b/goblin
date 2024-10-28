04dad2d6bf10b60dc6b21989852b0a5bfd8be681
kkent030315 <hrn832@protonmail.com>
2024-10-29 03:19:14 +0900

    Add `ReproInfo` and `ExDllCharacteristicsInfo` parsers
    - Added decent docs for `pe::debug` structs and constants
    - Added integration tests for `pe::debug::DebugData`

--- src/pe/debug.rs
+++ src/pe/debug.rs
@@ -90,17 +90,38 @@ pub struct DebugData<'a> {
     ///
     /// CodeView PDB 7.0 contains a GUID, an age value, and the path to the PDB file.
     /// This is commonly used in modern PDB files.
+    ///
+    /// [`IMAGE_DEBUG_TYPE_CODEVIEW`]
     pub codeview_pdb70_debug_info: Option<CodeviewPDB70DebugInfo<'a>>,
     /// Parsed CodeView PDB 2.0 (NB10) debug information, if available.
     ///
     /// CodeView PDB 2.0 includes a signature, an age value, and the path to the PDB file.
     /// It is used in older PDB formats.
+    ///
+    /// [`IMAGE_DEBUG_TYPE_CODEVIEW`]
     pub codeview_pdb20_debug_info: Option<CodeviewPDB20DebugInfo<'a>>,
     /// Visual C++ feature data, if available.
     ///
     /// This includes information about specific features or optimizations enabled
     /// in Visual C++ builds.
+    ///
+    /// [`IMAGE_DEBUG_TYPE_VC_FEATURE`]
     pub vcfeature_info: Option<VCFeatureInfo>,
+    /// Extended DLL characteristics information, if available.
+    ///
+    /// This data includes extended properties of the DLL that may affect
+    /// how the operating system handles the DLL, such as security features.
+    ///
+    /// [`IMAGE_DEBUG_TYPE_EX_DLLCHARACTERISTICS`]
+    pub ex_dll_characteristics_info: Option<ExDllCharacteristicsInfo>,
+    /// Reproducible build (Repro) information, if available.
+    ///
+    /// - **MSVC builds**: Contains a 32-byte hash stored directly in the raw data.
+    /// - **Clang builds**: Uses the `ImageDebugDirectory::time_date_stamp` as a hash,
+    ///   with no dedicated raw data.
+    ///
+    /// [`IMAGE_DEBUG_TYPE_REPRO`]
+    pub repro_info: Option<ReproInfo<'a>>,
 }
 
 impl<'a> DebugData<'a> {
@@ -154,6 +175,9 @@ impl<'a> DebugData<'a> {
         let codeview_pdb20_debug_info =
             CodeviewPDB20DebugInfo::parse_with_opts(bytes, iterator, opts)?;
         let vcfeature_info = VCFeatureInfo::parse_with_opts(bytes, iterator, opts)?;
+        let ex_dll_characteristics_info =
+            ExDllCharacteristicsInfo::parse_with_opts(bytes, iterator, opts)?;
+        let repro_info = ReproInfo::parse_with_opts(bytes, iterator, opts)?;
 
         Ok(DebugData {
             data,
@@ -161,6 +185,8 @@ impl<'a> DebugData<'a> {
             codeview_pdb70_debug_info,
             codeview_pdb20_debug_info,
             vcfeature_info,
+            ex_dll_characteristics_info,
+            repro_info,
         })
     }
 
@@ -476,7 +502,7 @@ impl<'a> CodeviewPDB20DebugInfo<'a> {
         let idd = idd.collect::<Result<Vec<_>, _>>()?;
         let idd = idd
             .iter()
-            .find(|idd| idd.data_type == IMAGE_DEBUG_TYPE_VC_FEATURE);
+            .find(|idd| idd.data_type == IMAGE_DEBUG_TYPE_CODEVIEW);
 
         if let Some(idd) = idd {
             // ImageDebugDirectory.pointer_to_raw_data stores a raw offset -- not a virtual offset -- which we can use directly
@@ -524,5 +550,309 @@ impl<'a> CodeviewPDB20DebugInfo<'a> {
             // Codeview20 not found
             return Ok(None);
         }
+    }
+}
+
+/// Represents the reproducible build (Repro) information extracted from a PE (Portable Executable) file.
+///
+/// The Repro information differs based on the compiler used to build the executable:
+/// - For MSVC (Microsoft Visual C++), the Repro information is written directly into the raw data as a 32-byte hash.
+/// - For Clang/(correctly, LLD linker), there is no dedicated raw data for the Repro information. Instead, the `ImageDebugDirectory::time_date_stamp`
+///   field functions as a hash, providing a unique identifier for the reproducible build.
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum ReproInfo<'a> {
+    /// Represents a hash stored in the `time_date_stamp` field.
+    ///
+    /// This variant is used primarily for executables built with Clang/LLD, where the
+    /// `time_date_stamp` acts as the Repro hash.
+    TimeDateStamp(u32),
+    /// Represents a buffer containing the 32-byte Repro hash.
+    ///
+    /// This variant is used for MSVC-built executables, where the Repro hash is directly
+    /// stored as raw data in the debug directory.
+    Buffer {
+        /// The length of the buffer containing the Repro data. For MSVC, this is typically 32 bytes long.
+        length: u32,
+        /// A reference to the buffer containing the Repro hash data.
+        buffer: &'a [u8],
+    },
+}
+
+impl<'a> ReproInfo<'a> {
+    pub fn parse(
+        bytes: &'a [u8],
+        idd: ImageDebugDirectoryIterator<'_>,
+    ) -> error::Result<Option<Self>> {
+        Self::parse_with_opts(bytes, idd, &options::ParseOptions::default())
+    }
+
+    pub fn parse_with_opts(
+        bytes: &'a [u8],
+        idd: ImageDebugDirectoryIterator<'_>,
+        opts: &options::ParseOptions,
+    ) -> error::Result<Option<Self>> {
+        let idd = idd.collect::<Result<Vec<_>, _>>()?;
+        let idd = idd
+            .iter()
+            .find(|idd| idd.data_type == IMAGE_DEBUG_TYPE_REPRO);
+
+        if let Some(idd) = idd {
+            let mut offset: usize = match opts.resolve_rva {
+                true => idd.pointer_to_raw_data as usize,
+                false => idd.address_of_raw_data as usize,
+            };
+
+            // Clang(LLD) produces no data = uses timestamp field instead
+            // MSVC(lin.exe) produces 36-byte data
+            if idd.size_of_data > 0 {
+                let length: u32 = bytes.gread_with(&mut offset, scroll::LE)?;
+                if let Some(buffer) = bytes.get(offset..offset + length as usize) {
+                    Ok(Some(Self::Buffer { length, buffer }))
+                } else {
+                    Err(error::Error::Malformed(format!(
+                        "ImageDebugDirectory seems corrupted: {:?}",
+                        idd
+                    )))
+                }
+            } else {
+                Ok(Some(Self::TimeDateStamp(idd.time_date_stamp)))
+            }
+        } else {
+            return Ok(None);
+        }
+    }
+}
+
+/// Represents extended DLL characteristics information.
+///
+/// This structure holds additional characteristics of a DLL that may influence
+/// how the operating system loads or manages the DLL, especially in terms of
+/// security features and optimizations. These characteristics can include
+/// settings related to ASLR (Address Space Layout Randomization), DEP (Data Execution Prevention),
+/// and other security-relevant attributes.
+#[repr(C)]
+#[derive(Debug, PartialEq, Copy, Clone, Default)]
+pub struct ExDllCharacteristicsInfo {
+    /// The extended characteristics of the DLL.
+    ///
+    /// This field is a bitmask of flags that define various security and performance
+    /// properties of the DLL. The specific flags are defined by the PE format specification.
+    ///
+    /// This field contains one or more bitflags of:
+    ///
+    /// - [`IMAGE_DLLCHARACTERISTICS_EX_CET_COMPAT`]
+    /// - [`IMAGE_DLLCHARACTERISTICS_EX_CET_COMPAT_STRICT_MODE`]
+    /// - [`IMAGE_DLLCHARACTERISTICS_EX_CET_SET_CONTEXT_IP_VALIDATION_RELAXED_MODE`]
+    /// - [`IMAGE_DLLCHARACTERISTICS_EX_CET_DYNAMIC_APIS_ALLOW_IN_PROC_ONLY`]
+    /// - [`IMAGE_DLLCHARACTERISTICS_EX_CET_RESERVED_1`]
+    /// - [`IMAGE_DLLCHARACTERISTICS_EX_CET_RESERVED_2`]
+    pub characteristics_ex: u32,
+}
+
+/// Enables Compatibility Enforcement Technology (CET) for the DLL to enhance
+/// security via control-flow integrity.
+pub const IMAGE_DLLCHARACTERISTICS_EX_CET_COMPAT: u32 = 0x1;
+/// Enforces CET in strict mode, increasing security measures against
+/// control-flow attacks but may impact compatibility.
+pub const IMAGE_DLLCHARACTERISTICS_EX_CET_COMPAT_STRICT_MODE: u32 = 0x2;
+/// Allows relaxed mode for Context IP Validation under CET, providing a balance
+/// between security and performance.
+pub const IMAGE_DLLCHARACTERISTICS_EX_CET_SET_CONTEXT_IP_VALIDATION_RELAXED_MODE: u32 = 0x4;
+/// Restricts the use of dynamic APIs to processes only, enhancing security by
+/// limiting external API calls under CET.
+pub const IMAGE_DLLCHARACTERISTICS_EX_CET_DYNAMIC_APIS_ALLOW_IN_PROC_ONLY: u32 = 0x8;
+/// Reserved for future use related to CET features. Not currently utilized.
+pub const IMAGE_DLLCHARACTERISTICS_EX_CET_RESERVED_1: u32 = 0x10;
+/// Reserved for future use related to CET features. Not currently utilized.
+pub const IMAGE_DLLCHARACTERISTICS_EX_CET_RESERVED_2: u32 = 0x20;
+
+impl<'a> ExDllCharacteristicsInfo {
+    pub fn parse(
+        bytes: &'a [u8],
+        idd: ImageDebugDirectoryIterator<'_>,
+    ) -> error::Result<Option<Self>> {
+        Self::parse_with_opts(bytes, idd, &options::ParseOptions::default())
+    }
+
+    pub fn parse_with_opts(
+        bytes: &'a [u8],
+        idd: ImageDebugDirectoryIterator<'_>,
+        opts: &options::ParseOptions,
+    ) -> error::Result<Option<Self>> {
+        let idd = idd.collect::<Result<Vec<_>, _>>()?;
+        let idd = idd
+            .iter()
+            .find(|idd| idd.data_type == IMAGE_DEBUG_TYPE_EX_DLLCHARACTERISTICS);
+
+        if let Some(idd) = idd {
+            // ImageDebugDirectory.pointer_to_raw_data stores a raw offset -- not a virtual offset -- which we can use directly
+            let mut offset: usize = match opts.resolve_rva {
+                true => idd.pointer_to_raw_data as usize,
+                false => idd.address_of_raw_data as usize,
+            };
+
+            let characteristics_ex: u32 = bytes.gread_with(&mut offset, scroll::LE)?;
+
+            Ok(Some(ExDllCharacteristicsInfo { characteristics_ex }))
+        } else {
+            return Ok(None);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::{
+        ExDllCharacteristicsInfo, ImageDebugDirectory, ReproInfo, VCFeatureInfo,
+        IMAGE_DEBUG_TYPE_CODEVIEW, IMAGE_DEBUG_TYPE_EX_DLLCHARACTERISTICS, IMAGE_DEBUG_TYPE_ILTCG,
+        IMAGE_DEBUG_TYPE_POGO, IMAGE_DEBUG_TYPE_REPRO, IMAGE_DEBUG_TYPE_VC_FEATURE,
+        IMAGE_DLLCHARACTERISTICS_EX_CET_COMPAT, IMAGE_DLLCHARACTERISTICS_EX_CET_COMPAT_STRICT_MODE,
+    };
+
+    const DEBUG_DIRECTORIES_TEST_MSVC_BIN: &[u8] =
+        include_bytes!("../../tests/bins/pe/debug_directories-msvc.exe.bin");
+    const DEBUG_DIRECTORIES_TEST_CLANG_LLD_BIN: &[u8] =
+        include_bytes!("../../tests/bins/pe/debug_directories-clang_lld.exe.bin");
+
+    #[test]
+    fn parse_debug_entries_iterator() {
+        let binary =
+            crate::pe::PE::parse(DEBUG_DIRECTORIES_TEST_MSVC_BIN).expect("Unable to parse binary");
+        assert_eq!(binary.debug_data.is_some(), true);
+        let debug_data = binary.debug_data.unwrap();
+        let entries = debug_data.entries().collect::<Result<Vec<_>, _>>();
+        assert_eq!(entries.is_ok(), true);
+        let entries = entries.unwrap();
+        let entries_expect = vec![
+            ImageDebugDirectory {
+                characteristics: 0x0,
+                time_date_stamp: 0x80AC7661,
+                major_version: 0x0,
+                minor_version: 0x0,
+                data_type: IMAGE_DEBUG_TYPE_CODEVIEW,
+                size_of_data: 0x38,
+                address_of_raw_data: 0x20c0,
+                pointer_to_raw_data: 0x4c0,
+            },
+            ImageDebugDirectory {
+                characteristics: 0x0,
+                time_date_stamp: 0x80AC7661,
+                major_version: 0x0,
+                minor_version: 0x0,
+                data_type: IMAGE_DEBUG_TYPE_VC_FEATURE,
+                size_of_data: 0x14,
+                address_of_raw_data: 0x20f8,
+                pointer_to_raw_data: 0x4f8,
+            },
+            ImageDebugDirectory {
+                characteristics: 0x0,
+                time_date_stamp: 0x80AC7661,
+                major_version: 0x0,
+                minor_version: 0x0,
+                data_type: IMAGE_DEBUG_TYPE_POGO,
+                size_of_data: 0x58,
+                address_of_raw_data: 0x210c,
+                pointer_to_raw_data: 0x50c,
+            },
+            ImageDebugDirectory {
+                characteristics: 0x0,
+                time_date_stamp: 0x80AC7661,
+                major_version: 0x0,
+                minor_version: 0x0,
+                data_type: IMAGE_DEBUG_TYPE_ILTCG,
+                size_of_data: 0x0,
+                address_of_raw_data: 0x0,
+                pointer_to_raw_data: 0x0,
+            },
+            ImageDebugDirectory {
+                characteristics: 0x0,
+                time_date_stamp: 0x80AC7661,
+                major_version: 0x0,
+                minor_version: 0x0,
+                data_type: IMAGE_DEBUG_TYPE_REPRO,
+                size_of_data: 0x24,
+                address_of_raw_data: 0x2164,
+                pointer_to_raw_data: 0x564,
+            },
+            ImageDebugDirectory {
+                characteristics: 0x0,
+                time_date_stamp: 0x80AC7661,
+                major_version: 0x0,
+                minor_version: 0x0,
+                data_type: IMAGE_DEBUG_TYPE_EX_DLLCHARACTERISTICS,
+                size_of_data: 0x4,
+                address_of_raw_data: 0x2188,
+                pointer_to_raw_data: 0x588,
+            },
+        ];
+        assert_eq!(entries, entries_expect);
+    }
+
+    #[test]
+    fn parse_debug_vcfeature() {
+        let binary =
+            crate::pe::PE::parse(DEBUG_DIRECTORIES_TEST_MSVC_BIN).expect("Unable to parse binary");
+        assert_eq!(binary.debug_data.is_some(), true);
+        let debug_data = binary.debug_data.unwrap();
+        assert_eq!(debug_data.vcfeature_info.is_some(), true);
+        let vcfeature_info = debug_data.vcfeature_info.unwrap();
+        let vcfeature_info_expect = VCFeatureInfo {
+            pre_vc_plusplus_count: 0,
+            c_and_cplusplus_count: 1,
+            guard_stack_count: 0,
+            sdl_count: 0,
+            guard_count: 0,
+        };
+        assert_eq!(vcfeature_info, vcfeature_info_expect);
+    }
+
+    #[test]
+    fn parse_debug_repro_msvc() {
+        let binary =
+            crate::pe::PE::parse(DEBUG_DIRECTORIES_TEST_MSVC_BIN).expect("Unable to parse binary");
+        assert_eq!(binary.debug_data.is_some(), true);
+        let debug_data = binary.debug_data.unwrap();
+        assert_eq!(debug_data.repro_info.is_some(), true);
+        let repro_info = debug_data.repro_info.unwrap();
+        let repro_info_expect = ReproInfo::Buffer {
+            length: 32,
+            buffer: &[
+                0x1F, 0x4F, 0x58, 0x9C, 0x3C, 0xEA, 0x00, 0x83, 0x3F, 0x57, 0x00, 0xCC, 0x36, 0xA7,
+                0x84, 0xDF, 0xF7, 0x7C, 0x70, 0xE0, 0xEF, 0x7A, 0xBA, 0x08, 0xD0, 0xA6, 0x8B, 0x7F,
+                0x61, 0x76, 0xAC, 0x80,
+            ],
+        };
+        assert_eq!(repro_info, repro_info_expect);
+    }
+
+    #[test]
+    fn parse_debug_repro_clang_lld() {
+        let binary = crate::pe::PE::parse(DEBUG_DIRECTORIES_TEST_CLANG_LLD_BIN)
+            .expect("Unable to parse binary");
+        assert_eq!(binary.debug_data.is_some(), true);
+        let debug_data = binary.debug_data.unwrap();
+        assert_eq!(debug_data.repro_info.is_some(), true);
+        let repro_info = debug_data.repro_info.unwrap();
+        let repro_info_expect = ReproInfo::TimeDateStamp(0xDB2F3908);
+        assert_eq!(repro_info, repro_info_expect);
+    }
+
+    #[test]
+    fn parse_debug_exdllcharacteristics() {
+        let binary =
+            crate::pe::PE::parse(DEBUG_DIRECTORIES_TEST_MSVC_BIN).expect("Unable to parse binary");
+        assert_eq!(binary.debug_data.is_some(), true);
+        let debug_data = binary.debug_data.unwrap();
+        assert_eq!(debug_data.ex_dll_characteristics_info.is_some(), true);
+        let ex_dll_characteristics_info = debug_data.ex_dll_characteristics_info.unwrap();
+        let ex_dll_characteristics_info_expect = ExDllCharacteristicsInfo {
+            characteristics_ex: IMAGE_DLLCHARACTERISTICS_EX_CET_COMPAT
+                | IMAGE_DLLCHARACTERISTICS_EX_CET_COMPAT_STRICT_MODE,
+        };
+        assert_eq!(
+            ex_dll_characteristics_info,
+            ex_dll_characteristics_info_expect
+        );
     }
 }

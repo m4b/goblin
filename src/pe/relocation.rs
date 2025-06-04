c2a76536b3fc2ac76bf800420182b9f056819a2b
BinFlip <code@binflip.rs>
2025-06-04 11:11:17 -0700

    pe.relocation.RelocationData.parse_with_opts - Possible out of bound memory access

--- src/pe/relocation.rs
+++ src/pe/relocation.rs
@@ -347,6 +347,15 @@ impl<'a> RelocationData<'a> {
                         dd.virtual_address
                     ))
                 })?;
+        
+        // Ensure that the offset does not exceed the length of the bytes slice
+        if offset >= bytes.len() {
+            return Err(error::Error::Malformed(format!(
+                "base reloc offset {:#x} exceeds the bounds of the bytes size {:#x}",
+                offset, bytes.len()
+            )));
+        }
+
         let bytes = bytes[offset..]
             .pread::<&[u8]>(dd.size as usize)
             .map_err(|_| {
